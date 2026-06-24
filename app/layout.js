a2770e3ac8a99682845be21d6142a23d6e1a1704
ipomoea2000 <60794785+ipomoea2000@users.noreply.github.com>
2026-06-24 12:56:19 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Sweetpotato AI Demo",
+  description: "LSU AgCenter Fertility Tool",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+``
